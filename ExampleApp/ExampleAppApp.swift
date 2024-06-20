e9cf454c57993630e15638c5942c7098af232881
Filip Štěpánek <>
2024-06-20 08:47:12 +0200

    New blank project.

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Filip Štěpánek on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
